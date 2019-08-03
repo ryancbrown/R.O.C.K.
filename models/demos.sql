2b4c33b6a3f2176787cf57e3a22fa4c82d6e5ef8
Kim Graff <kimagraff@gmail.com>
2019-08-03 12:01:54 -0400

    trying to fix travis

--- models/demos.sql
+++ models/demos.sql
@@ -1,10 +1,1 @@
-INSERT INTO Events (event_id) VALUES ('1');
-INSERT INTO Events (event_name) VALUES ('queer POC parties');
-INSERT INTO Events (event_type) VALUES ('Music');
-INSERT INTO Events (event_date) VALUES ('1/1/2022');
-INSERT INTO Events (event_link) VALUES ('https://www.timeout.com/newyork/lgbt/best-queer-poc-parties');
-INSERT INTO Events (event_location) VALUES ('Manhattan');
-INSERT INTO Events (event_description) VALUES ('Find the best parties in New York City for Queer POCs, featuring artists, music, and the best shows by them.');
-INSERT INTO Events (event_image) VALUES ('https://media.timeout.com/images/103902787/1372/772/image.jpg');
-INSERT INTO Events (price) VALUES ('0');
-INSERT INTO Events (attendence) VALUES ('0')
+INSERT INTO `Events` (event_id, event_name, event_type, event_date, event_link, event_location, event_description, event_image, price, attendence) VALUES ('1', 'queer POC parties', 'Music', '1/1/2022', 'https://www.timeout.com/newyork/lgbt/best-queer-poc-parties', 'NYC', 'Find the best parties in NYC for Queer POCs', 'https://media.timeout.com/images/103902787/1372/772/image.jpg', '0', '0');
