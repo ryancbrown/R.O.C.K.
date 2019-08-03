404de07a2efbad76a2d917944f337da233d27805
Kim Graff <kimagraff@gmail.com>
2019-08-03 11:20:51 -0400

    updating events

--- models/Events.js
+++ models/Events.js
@@ -1,47 +1,53 @@
 /* eslint-disable camelcase */
 module.exports = function(sequelize, DataTypes) {
-  var Events = sequelize.define("Events", {
-    event_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true
+  var Events = sequelize.define(
+    "Events",
+    {
+      event_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+      },
+      event_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      event_type: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      event_date: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      event_link: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      event_location: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      event_description: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      event_image: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+      },
+      attendence: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      }
     },
-    event_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    event_type: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    event_date: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    event_link: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    event_location: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    event_description: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    event_image: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    price: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0
-    },
-    attendence: {
-      type: DataTypes.INTEGER,
-      allowNull: true
+    {
+      timestamps: false
     }
-  });
+  );
   return Events;
 };
