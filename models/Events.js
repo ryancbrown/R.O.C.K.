190a253b88afc80b644badcdd54a6c4dad4370e4
Kim Graff <kimagraff@gmail.com>
2019-08-05 21:49:57 -0400

    update

--- models/Events.js
+++ models/Events.js
@@ -9,37 +9,37 @@ module.exports = function(sequelize, DataTypes) {
         primaryKey: true
       },
       event_name: {
-        type: DataTypes.STRING,
-        allowNull: false
+        type: DataTypes.STRING
+        // allowNull: false
       },
       event_type: {
-        type: DataTypes.STRING,
-        allowNull: false
+        type: DataTypes.STRING
+        // allowNull: false
       },
       event_date: {
-        type: DataTypes.STRING,
-        allowNull: false
+        type: DataTypes.STRING
+        // allowNull: false
       },
       event_link: {
-        type: DataTypes.STRING,
-        allowNull: false
+        type: DataTypes.STRING
+        // allowNull: false
       },
       event_location: {
-        type: DataTypes.STRING,
-        allowNull: true
+        type: DataTypes.STRING
+        // allowNull: true
       },
       event_description: {
-        type: DataTypes.STRING,
-        allowNull: false
+        type: DataTypes.STRING
+        // allowNull: false
       },
       event_image: {
-        type: DataTypes.STRING,
-        allowNull: false
+        type: DataTypes.STRING
+        // allowNull: false
       },
       event_price: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: "$0"
+        type: DataTypes.STRING
+        // allowNull: false,
+        // defaultValue: "$0"
       }
     },
     {
